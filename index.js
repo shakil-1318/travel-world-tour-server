1d72f3effd28b654e434ee014b786e1c0feaf853
shakil-1318 <shakiljust21@gmail.com>
2021-11-21 18:18:17 +0600

    yea

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware it is very important
+// middleware
 app.use(cors());
 app.use(express.json());
 
